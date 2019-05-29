2bac28848de301496fcad4fc389da2e54e403d35
syahirahadibah43 <51169274+syahirahadibah43@users.noreply.github.com>
2019-05-30 01:30:21 +0800

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("I'm adding code to my project");
+		System.out.println("Adding code from GitHub");
 
 	}
 
